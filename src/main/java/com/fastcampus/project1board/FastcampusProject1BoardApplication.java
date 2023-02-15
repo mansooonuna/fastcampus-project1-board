2381f1c30cb053a9284381998f73424db3309580
mansooonuna <parkdasom95@gmail.com>
2023-02-15 22:35:31 +0900

    #6 - 스프링 부트 프로젝트 시작
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/fastcampus/project1board/FastcampusProject1BoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.project1board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusProject1BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcampusProject1BoardApplication.class, args);
+	}
+
+}
